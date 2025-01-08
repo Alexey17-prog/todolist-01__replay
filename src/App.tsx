ca37c5bf45d166c9468dc42ca8a52c9bbbbfdae6
Alexey17 <axelymasysadm@gmail.com>
2025-01-08 13:58:11 +0300

    овторение 1 спринт туду

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+import {TodolistItem} from "./TodolistItem.tsx";
+
+export type Task = {
+  id: number
+  title: string
+  isDone: boolean
+}
+
+export const App = () => {
+  const tasks1: Task[] = [
+    { id: 1, title: 'HTML&CSS', isDone: true },
+    { id: 2, title: 'JS', isDone: true },
+    { id: 3, title: 'ReactJS', isDone: false },
+  ]
+
+
+
+  return (
+      <div className="app">
+       <TodolistItem title={'TodolistItem1'} tasks={tasks1} date={"01.01.25"}/>
+      </div>
+  )
+}
+
+export default App
